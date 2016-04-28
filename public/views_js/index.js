6b6420e28903384d156ca18f292d0f4e720b3c6a
Allen Kim <allenkim95@gmail.com>
2016-04-27 22:11:09 -0400

    Add basic rerouting to game for only users going in from the start game button

--- public/views_js/index.js
+++ public/views_js/index.js
@@ -1,6 +1,27 @@
+var waiting = false;	
+var ready = false;
+
 $( document ).ready(function(){
 	$('#join').click(function(){
-		$('#join').val("Stop Waiting");
-		$('#waitingText span').text('Waiting');
+		if (waiting){
+			waiting = false;
+			$('#join').val("Join Game");
+			$('#waitingText span').text('Some random text');
+		}
+		else{
+			waiting = true;
+			$('#join').val("Stop Waiting");
+			$('#waitingText span').text('Waiting');
+		}
 	}); 
+	$('#start').click(function(){
+		ready = true;
+		$.post("http://localhost:3000/enter",{ready:ready},function(data){        
+			if(data==='done')           
+				{
+					window.location.href="/game";
+				}
+		});
+	});
 });
+
--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 var ejs = require('ejs');
 var express = require('express');
 var app = express();
+var session = require('express-session');
+var bodyParser = require('body-parser');
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
@@ -11,24 +13,47 @@ app.set("view options", { layout: false });
 app.set('port', 3000);  
 app.set('ipaddr', "127.0.0.1");
 app.use(express.static(__dirname + '/public'));
+
+// Sessions allow us to verify if users can join the game
+app.use(session({
+	name: 'server-session-name',
+	secret: 'my express secret',
+	saveUninitialized: true,
+	resave: true,
+}));
+
+// Body parser allows us to read the body from the POST request from client`
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 app.engine('html', ejs.renderFile);
 
 app.get('/', function(req, res){
-  res.render('index.html');
+	if (typeof req.session.ready === 'undefined') {
+		req.session.ready = false;
+	}
+	return res.render('index.html');
 });
 
+// Accepts POST request from client
+app.post('/enter',function(req,res){
+	sess = req.session;
+	sess.ready=req.body.ready;
+	res.end('done');
+});
 
 app.get('/game', function(req, res){
-  res.render('game.html');
+	if (req.session.ready)
+		res.render('game.html');
+	else
+		res.redirect('/');
 });
 
-
 io.on('connection', function(socket){
-	console.log('a user connected');
+	console.log('user ' +  socket.id + ' connected');
 
-  socket.on('chat message', function(msg){
-    io.emit('chat message', msg);
-  });
+	socket.on('chat message', function(msg){
+		io.emit('chat message', msg);
+	});
 
 	socket.on('disconnect',function(){
 		console.log('a user disconnected');
@@ -36,5 +61,5 @@ io.on('connection', function(socket){
 });
 
 http.listen(3000, function(){
-  console.log('listening on *:3000');
+	console.log('listening on *:3000');
 });

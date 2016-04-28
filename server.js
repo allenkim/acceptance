e66aacd16d4ad0207fc33807c3fe134cdef3c04c
Allen Kim <allenkim95@gmail.com>
2016-04-28 14:45:03 -0400

    Improve game joining function, only one tab per session allowed to join, only allows clients who waited through the home page to join

--- server.js
+++ server.js
@@ -27,6 +27,11 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.engine('html', ejs.renderFile);
 
+// sessionToSockets is a map from a session ID to an array of socket IDs
+// socketToSession is a map from socket ID to session ID
+var sessionToSockets = {};
+var socketToSession = {};
+
 app.get('/', function(req, res){
 	if (typeof req.session.ready === 'undefined') {
 		req.session.ready = false;
@@ -34,29 +39,93 @@ app.get('/', function(req, res){
 	return res.render('index.html');
 });
 
-// Accepts POST request from client
+// This POST is called when the client first connects with their socket
+// This updates the maps of sessions and sockets
+app.post('/connect',function(req,res){
+	var sessID = req.sessionID;
+	var socketID = req.body.socketID;
+	socketToSession[socketID] = sessID;
+	if (sessID in sessionToSockets){
+		sessionToSockets[sessID].push(socketID);
+	}
+	else{
+		sessionToSockets[sessID] = [socketID];
+	}
+});
+
+// This POST is called when the client is entering the game
 app.post('/enter',function(req,res){
-	sess = req.session;
-	sess.ready=req.body.ready;
+	req.session.ready = req.body.ready;
 	res.end('done');
 });
 
+// A client can only join the game if they are ready, meaning they have waited and the players have filled up
+// and if they are part of the approved sessionIDs
 app.get('/game', function(req, res){
-	if (req.session.ready)
+	if (req.session.ready){
+		req.session.ready = false;
 		res.render('game.html');
-	else
-		res.redirect('/');
+	}
+	else{
+		res.send('You are not authorized to join the game!');
+	}
 });
+
+// Variables to keep track of player count
+var MAX_NUM_PLAYERS = 2;
+var playersWaiting = [];
 
 io.on('connection', function(socket){
 	console.log('user ' +  socket.id + ' connected');
+	socket.emit('connect');
+	socket.on('waiting', function(){
+		var sessID = socketToSession[socket.id];
+		var waitingAllowed = true;
+		for (var i = 0; i < playersWaiting.length; i++){
+			if (socketToSession[playersWaiting[i]] == sessID){
+				waitingAllowed = false;
+				break;
+			}
+		}
+		if (waitingAllowed){
+			playersWaiting.push(socket.id);
+		}
+		if (playersWaiting.length == MAX_NUM_PLAYERS){
+			playersWaiting.forEach(function(id){
+				io.to(id).emit('enter game');
+			});
+		}
+		else{
+			io.to(socket.id).emit('update waiting', {numPlayersWaitingFor: MAX_NUM_PLAYERS - playersWaiting.length, waitingAllowed: waitingAllowed});
+		}
+	});
+
+	socket.on('stop waiting', function(){
+		var idx = playersWaiting.indexOf(socket.id);
+		if (idx != -1){
+			playersWaiting.splice(idx,1);
+			playersWaiting.forEach(function(id){
+				io.to(id).emit('update waiting', {numPlayersWaitingFor: MAX_NUM_PLAYERS - playersWaiting.length, waitingAllowed: true});
+			});
+		}
+	});
 
 	socket.on('chat message', function(msg){
 		io.emit('chat message', msg);
 	});
 
 	socket.on('disconnect',function(){
-		console.log('a user disconnected');
+		console.log('user ' + socket.id + ' disconnected');
+		var idx = playersWaiting.indexOf(socket.id);
+		if (idx != -1)
+			playersWaiting.splice(idx,1);
+		var sessID = socketToSession[socket.id];
+		delete socketToSession[socket.id];
+		if (sessID in sessionToSockets){
+			var sockIdx = sessionToSockets[sessID].indexOf(socket.id);
+			if (sockIdx != -1)
+				sessionToSockets[sessID].splice(sockIdx,1);
+		}
 	});
 });
 
